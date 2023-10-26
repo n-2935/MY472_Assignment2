63ea49dcc0f01619fce7f4c9d535f33d5152d868
Noor Aly <N.Asharaf-Aly@lse.ac.uk>
2023-10-26 16:23:34 +0100

    Update Squared_Transformation.R
    R function with 2 arguments

--- Squared_Transformation.R
+++ Squared_Transformation.R
@@ -1,17 +1,16 @@
-## Function with one argument data that squares data and returns squared data
+## Function with 2 arguments data that squares data if "square" requested and returns squared data
 
-squared_data <- function(data) {
-  
-  # Square each element of the vector
-  squared_data <- data ^ 2
-  
-  # Return the transformed data
-  return(squared_data)
+squared_data <- function(data,power) {
+  if (power == "square") {
+    # Square each element of the vector
+    squared_data <- data^2
+    return(squared_data)
+  } else {
+    return("Not available")
+  }
 }
 
-
-# Example with the "mtcars" dataset
-
+#Example with mtcars dataset
 # Load the mtcars dataset
 data(mtcars)
 
@@ -19,7 +18,10 @@ data(mtcars)
 head(mtcars)
 
 # Apply the function to the 'cyl' column
-squared_cyl <- squared_data(mtcars$cyl)
+squared_cyl <- squared_data(mtcars$cyl,"square")
 
 # Print the result
 print(squared_cyl)
+
+
+
